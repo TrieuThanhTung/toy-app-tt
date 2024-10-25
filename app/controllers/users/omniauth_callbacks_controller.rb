e1968fa4d84a12414c9f56d3be17c07cb3b3da4d
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-10-25 14:07:01 +0700

    setting facebook api

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include SessionsHelper
   def facebook
-    # @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    # if @user.persisted?
-    #   sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-    #   set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    # else
-    #   session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
-    #   redirect_to new_user_registration_url
-    # end
-
-    logger.debug "log here -------------- #{auth} -----\n #{auth.info}"
-    redirect_to root_path
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    login_redirect(user, "Facebook")
   end
 
   def github
@@ -26,7 +16,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def login_redirect(user, kind_sso)
-    if user
+    if user.persisted?
       log_in(user)
       flash[:notice] = t "devise.omniauth_callbacks.success", kind: kind_sso
       redirect_to user_url(user.id), event: :authentication
@@ -36,11 +26,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-   def auth
-     @auth ||= request.env["omniauth.auth"]
-   end
+  def auth
+    @auth ||= request.env["omniauth.auth"]
+  end
 
-   def failure
+  def failure
     redirect_to root_path
   end
 end
