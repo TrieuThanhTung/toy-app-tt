21f7e7a06e32cf4478d77d39d12d0512a0222d84
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-10-29 15:36:37 +0700

    fix tests

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -2,15 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Provider, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
   let(:user) {
     create(:user)
   }
-
-  it "Provider is valid with valid attributes" do
-    should belong_to(:user)
-    should validate_presence_of(:name)
-    should validate_presence_of(:uid)
-  end
 
   it "Provider is not valid without a user" do
     should belong_to(:user)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "user is valid with valid attributes" do
-    should validate_presence_of(:name)
-    should validate_presence_of(:email)
-    should validate_presence_of(:password)
-  end
-
   it "user is valid without age" do
     user = User.new(name: "test-name", email: "test-email@gmai.com", password: "password")
     expect(user).to be_valid
@@ -17,9 +11,10 @@ RSpec.describe User, type: :model do
     should validate_presence_of(:name)
   end
 
-  it "user is invalid with name less than 6 character" do
-    user = User.new(name: "test", age: 20, email: "test-email@gmai.com", password: "password")
-    expect(user).to_not be_valid
+  it "user is invalid with name's length between 6 -> 255" do
+    should validate_length_of(:name)
+             .is_at_least(6)
+             .is_at_most(255)
   end
 
   it "user is invalid without email" do
@@ -31,7 +26,6 @@ RSpec.describe User, type: :model do
   end
 
   it "user is invalid with password less than 6 characters" do
-    user = User.new(name: "test", age: 20, email: "test-email@gmai.com", password: "pass")
-    expect(user).to_not be_valid
+    should validate_length_of(:password).is_at_least(6)
   end
 end
