bca50d917ccba1771d24f7d3da27ddbd2e2c9403
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-11-08 14:57:46 +0700

    add rspec tests

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Reaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:micropost) }
+    it { should validate_presence_of(:reaction_type)}
+  end
 end
+
