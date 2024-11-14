b98eea6b28e6dccbd67ec8d4cb133df6b70b66e9
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-11-15 00:09:44 +0700

    add model rspec tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
+  belongs_to :room
 
   validates :content, presence: true
 
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Participant, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:room) }
+  end
 end
