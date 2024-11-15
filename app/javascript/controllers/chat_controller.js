9ae80663ed64d0d1fafae6e93e4501b56de830c2
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-11-15 10:39:06 +0700

    fix ui

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -27,39 +27,41 @@ export default class extends Controller {
             if (data.message.sender_id.toString() === senderId) {
                 messages.insertAdjacentHTML("beforeend",
                     `<div id="message_${data.message.id}" class="message-container sender">
-                            <div class="overlay" data-chat-target="overlay" id="overlay_${data.message.id}">
-                              <div id="message_edit_${data.message.id}" class="edit-message-container">
-                                <div class="header-edit">
-                                  <span class="icon-close" data-chat-target="closeEditBtn">
-                                    <i class="fa-solid fa-xmark" data-message-id="${data.message.id}" data-action="click->chat#openOverlay">
-                                    </i>
-                                  </span>
+                            <div class="message-container-sender">
+                                <div class="overlay" data-chat-target="overlay" id="overlay_${data.message.id}">
+                                  <div id="message_edit_${data.message.id}" class="edit-message-container">
+                                    <div class="header-edit">
+                                      <span class="icon-close" data-chat-target="closeEditBtn">
+                                        <i class="fa-solid fa-xmark" data-message-id="${data.message.id}" data-action="click->chat#openOverlay">
+                                        </i>
+                                      </span>
+                                    </div>
+                                    <form class="message-form-container" data-message-id="${data.message.id}" data-action="submit->chat#updateMessage"
+                                            accept-charset="UTF-8" method="post">
+                                          <input type="hidden" name="_method" value="put" autocomplete="off">
+                                          <input type="hidden" name="authenticity_token" value="RZ-260Qv6bibBOdYHHTvTn49VAwii2A-42na9TXiRTWmH1JUhV-ra3VTzLWWWKDDQ3Z72o19CQciwBU9rfvwyA"
+                                          autocomplete="off">
+                                          <input value="${data.message.content}" name="content" placeholder="Chat here..."
+                                          class="message-form-input" id="edit_input_${data.message.id}" data-chat-target="textinputEdit" type="text"
+                                          id="content">
+                                          <input type="submit" name="commit" value="Edit" class="comment-button message-form-submit"
+                                          data-disable-with="Edit">
+                                    </form>
+                                  </div>
                                 </div>
-                                <form class="message-form-container" data-message-id="${data.message.id}" data-action="submit->chat#updateMessage"
-                                        accept-charset="UTF-8" method="post">
-                                      <input type="hidden" name="_method" value="put" autocomplete="off">
-                                      <input type="hidden" name="authenticity_token" value="RZ-260Qv6bibBOdYHHTvTn49VAwii2A-42na9TXiRTWmH1JUhV-ra3VTzLWWWKDDQ3Z72o19CQciwBU9rfvwyA"
-                                      autocomplete="off">
-                                      <input value="${data.message.content}" name="content" placeholder="Chat here..."
-                                      class="message-form-input" id="edit_input_${data.message.id}" data-chat-target="textinputEdit" type="text"
-                                      id="content">
-                                      <input type="submit" name="commit" value="Edit" class="comment-button message-form-submit"
-                                      data-disable-with="Edit">
-                                </form>
-                              </div>
+                                <div class="more-options-message">
+                                  <button class="more-options-message-button">...</button>
+                                  <ul class="more-options">
+                                    <li class="item-option" data-chat-target="editMessage" data-message-id="${data.message.id}" data-action="click->chat#openOverlay">
+                                      Edit
+                                    </li>
+                                    <li class="item-option">
+                                        <a data-turbo-method="delete" data-turbo-confirm="You sure?" href="/users/${recipientUser}/messages/${data.message.id}">Delete</a>
+                                    </li>
+                                  </ul>
+                                </div>
+                                <span class="message sender">${data.message.content}</span>
                             </div>
-                            <div class="more-options-message">
-                              <button class="more-options-message-button">...</button>
-                              <ul class="more-options">
-                                <li class="item-option" data-chat-target="editMessage" data-message-id="${data.message.id}" data-action="click->chat#openOverlay">
-                                  Edit
-                                </li>
-                                <li class="item-option">
-                                    <a data-turbo-method="delete" data-turbo-confirm="You sure?" href="/users/${recipientUser}/messages/${data.message.id}">Delete</a>
-                                </li>
-                              </ul>
-                            </div>
-                            <span class="message sender">${data.message.content}</span>
                         </div>`
                 )
             } else {
@@ -92,6 +94,19 @@ export default class extends Controller {
                         case "update":
                             const updatedMessage = document.querySelector(`#message_${data.message.id} .message`)
                             updatedMessage.textContent = data.message.content
+                            const message = document.querySelector(`#message_${data.message.id}`)
+                            const infoEditMessage = document.querySelector(`#message_${data.message.id} .info-edit-message`)
+                            if (!infoEditMessage) {
+                                message.insertAdjacentHTML("beforeend", `
+                                    <div class="info-edit-message">
+                                        Edited
+                                    </div>
+                                `
+                                )
+                            } else {
+                                infoEditMessage.textContent = "Edited"
+                            }
+
                             if (data.message.sender_id.toString() !== recipientUser) {
                                 const overlay = document.getElementById(`overlay_${data.message.id}`)
                                 overlay.style.display = 'none'
